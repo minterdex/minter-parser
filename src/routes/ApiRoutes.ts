201511049bdeaab401bb97f44e7205afcdbc9bc4
Angelo <angelo@rcstart.it>
2019-04-24 17:59:15 +0200

    initial transaction api

--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -1,13 +1,16 @@
 import  * as express from "express";
 import { StatusController } from "../controllers/StatusController";
+import { TransactionController } from "../controllers/TransactionController";
 
 const router = express.Router();
 
-
 const statusController = new StatusController();
+const transactionController = new TransactionController();
 
 // URLs for transactions
 router.get("/", statusController.getStatus);
+router.get("/transactions", transactionController.readAllTransactions);
+router.get("/transactions/:transactionId", transactionController.readOneTransaction);
 
 export {
     router
